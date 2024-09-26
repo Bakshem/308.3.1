99863f804e4ea7037e5f84331153dedc1713fa30
elifb <elifbaksi@gmail.com>
2024-09-26 18:23:54 -0400

    adding more files

---
+++ countDownWithCondition.js
@@ -0,0 +1,12 @@
+let userDefinedNumber = 8;
+while (userDefinedNumber > 0 ) {
+    if ( userDefinedNumber === 5 ) {
+        console.log ("Almost there!");
+    } else if (userDefinedNumber === 1 ) {
+        console.log ("Liftoff!")
+        }
+    else {
+        console.log(userDefinedNumber);
+    }
+    userDefinedNumber--;
+}
--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,40 @@
+/*
+    Engineering method :
+
+    Explore :   ask questions      /  can there be negatives?  / Will there be other types?
+                test case           / 0    /    null=> what do I return?
+
+
+    Vrainstorm : multiple solutions
+                 pick the best option
+
+                
+    Plan :      plan around  the best solution
+                step by step
+
+                iterate from 0 to a number (50, 100, 1000)
+                    check if a  number us divisible by 5
+                        if it is log 'fizz'
+                    check if a  number us divisible by 3
+                        if it is log 'buzz'
+                    check if a  number us divisible by 5 & 3
+                        if it is log 'fizz buzz'
+
+
+    Execute :   writing code
+
+*/
+
+
 for ( let a = 1; a <= 100; a++ ){
-    if ( a %3 === 0 ) {
-        console.log("Fizz")
+    if (a %3 === 0 && a %5 === 0 ) {
+        console.log("Fizz Buzz")
     } else if ( a %5 === 0 ){
         console.log("Buzz")
-    } else if ( a %3 === 0 && a %5 === 0 ) {
-        console.log("Fizz Buzz")
+    } else if ( a %3 === 0 ) {
+        console.log("Fizz")
     } else {
         console.log(a)
     }
 }
+
